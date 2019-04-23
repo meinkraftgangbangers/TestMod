616929866e2535f76d8fb3843f1e45fa84629e85
amBready <37811228+amBready@users.noreply.github.com>
2019-04-23 00:11:36 -0500

    Ore generation
    yeeT

--- src/main/java/meinkraftgangbangers/gasymod/blocks/GasyOre.java
+++ src/main/java/meinkraftgangbangers/gasymod/blocks/GasyOre.java
@@ -1,7 +1,15 @@
 package meinkraftgangbangers.gasymod.blocks;
 
+import meinkraftgangbangers.gasymod.init.ModItems;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class GasyOre extends BlockBase {
 
@@ -14,4 +22,17 @@ public class GasyOre extends BlockBase {
         setHarvestLevel("pickaxe", 2);
         setLightLevel(0.1F);
     }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return ModItems.GASY;
+    }
+
+    @Override
+    public int quantityDropped(Random rand) {
+        int[] values = {1, 2, 2, 2, 2, 3, 3, 3, 4, 0};
+
+        return values[rand.nextInt(values.length - 1)];
+    }
+
 }
