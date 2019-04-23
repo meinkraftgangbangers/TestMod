5fdd21618120a600c8bd2517e19eabb5a0909b77
Austin Leuenhagen <fireball.al79@gmail.com>
2019-04-22 21:52:40 -0500

    add new recipes

---
+++ src/main/java/meinkraftgangbangers/gasymod/init/ModRecipes.java
@@ -0,0 +1,13 @@
+package meinkraftgangbangers.gasymod.init;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModRecipes {
+
+    public static void init() {
+        GameRegistry.addSmelting(ModItems.GASY, new ItemStack(ModItems.GASY_INGOT, 1), 2.5f);
+        GameRegistry.addSmelting(ModBlocks.GASY_ORE, new ItemStack(ModItems.GASY_INGOT, 2), 2.5f);
+    }
+
+}
