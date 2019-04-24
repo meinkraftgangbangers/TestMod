c016fbf5bed6100095722ff919979a110118117a
Austin Leuenhagen <fireball.al79@gmail.com>
2019-04-23 23:21:56 -0500

    balancing stuff, added dust

--- src/main/java/meinkraftgangbangers/gasymod/blocks/GasyOre.java
+++ src/main/java/meinkraftgangbangers/gasymod/blocks/GasyOre.java
@@ -5,6 +5,8 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 import java.util.Random;
 
@@ -27,17 +29,38 @@ public class GasyOre extends BlockBase {
     @Override
     public int quantityDroppedWithBonus(int fortune, Random rand) {
         if (fortune > 0) {
+            int i = rand.nextInt(fortune + 2) - 1;
+
+            if (i < 0) {
+                i = 0;
+            }
+
+            return this.quantityDropped(rand) * (i + 1);
+        }
+        else {
+            return this.quantityDropped(rand);
+        }
+
+        /*if (fortune > 0) {
             int r = rand.nextInt(fortune) + fortune;
             return quantityDropped(rand) + r;
         }
         else {
             return quantityDropped(rand);
-        }
+        }*/
     }
 
     @Override
     public int quantityDropped(Random rand) {
-        int[] values = {1, 2, 2, 2, 2, 3, 3};
-        return values[rand.nextInt(values.length)];
+        /*int[] values = {1, 2, 2, 2, 2, 3, 3};
+        return values[rand.nextInt(values.length)];*/
+        return 1 + rand.nextInt(2);
+    }
+
+    @Override
+    public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune) {
+        Random rand = new Random();
+        //THE RETURN VALUE IS HOW MANY XP ORBS IT WILL DROP WHEN THE BLOCK IS BROKEN.
+        return rand.nextInt(3) + 1;
     }
 }
--- src/main/java/meinkraftgangbangers/gasymod/init/ModRecipes.java
+++ src/main/java/meinkraftgangbangers/gasymod/init/ModRecipes.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModRecipes {
 
     public static void init() {
-        GameRegistry.addSmelting(ModItems.GASY, new ItemStack(ModItems.GASY_INGOT, 1), 1.5f);
+        GameRegistry.addSmelting(ModItems.GASY_DUST, new ItemStack(ModItems.GASY_INGOT, 1), 2.5f);
         GameRegistry.addSmelting(ModBlocks.GASY_ORE, new ItemStack(ModItems.GASY_INGOT, 1), 2.5f);
     }
 
