cab685da6a7953c5168cc2446e981cdc429b478c
Austin Leuenhagen <fireball.al79@gmail.com>
2019-04-24 20:16:27 -0500

    first release

--- src/main/java/meinkraftgangbangers/gasymod/util/Reference.java
+++ src/main/java/meinkraftgangbangers/gasymod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "gasymod";
     public static final String NAME = "Gasy Mod";
-    public static final String VERSION = "0.0.69";
+    public static final String VERSION = "0.1";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "meinkraftgangbangers.gasymod.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "meinkraftgangbangers.gasymod.proxy.CommonProxy";
